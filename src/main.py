3ab4e2a9e1b4cad6e32cc8f5652674e5df1abc0d
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2021-06-17 13:55:34 +0300

    train - wip

--- src/main.py
+++ src/main.py
@@ -1,17 +1,35 @@
-import supervisely_lib as sly
-import globals as g
 import os
 import numpy as np
+import subprocess
+import supervisely_lib as sly
+import globals as g
 
 
 def download_data(image_id, is_test=False):
+    test_ann = None
     if is_test is True:
         img_dir = g.test_dir
     else:
         img_dir = g.train_dir
 
     info = g.api.image.get_info_by_id(image_id)
-    img_path = os.path.join(img_dir, f"{image_id}{info.name}")
+    img_path = os.path.join(img_dir, f"{image_id}{sly.fs.get_file_ext(info.name)}")
+    if not sly.fs.file_exists(img_path):
+        g.api.image.download_path(image_id, img_path)
+
+    ann_path = os.path.join(img_dir, f"{image_id}.json")
+    ann_json = g.api.annotation.download(image_id).annotation
+    sly.json.dump_json_file(ann_json, ann_path)
+
+    ann = sly.Annotation.from_json(ann_json, g.project_meta)
+    machine_mask = np.zeros(shape=ann.img_size + (3,), dtype=np.uint8)
+    for label in ann.labels:
+        if not label.obj_class.name.endswith("_prediction"):
+            #@TODO: skip prediction by object tag "prediction"
+            label.geometry.draw(machine_mask, color=g.machine_map[label.obj_class.name])
+
+    sly.image.write(os.path.join(g.machine_masks_dir, f"{image_id}.png"), machine_mask)
+    return ann
 
     # if is_test:
     #     img_dir = g.test_img_dir
@@ -36,6 +54,14 @@ def download_data(image_id, is_test=False):
 def add_to_train(api: sly.Api, task_id, context, state, app_logger):
     image_id = context['imageId']
     _ = download_data(image_id, is_test=False)
+
+
+    # generate_trained_project.py
+    train_cmd = "/ilastik-build/ilastik-1.4.0b14-Linux/bin/python "
+    docker_inspect_out = subprocess.Popen([docker_inspect_cmd],
+                                          shell=True, executable="/bin/bash",
+                                          stdout=subprocess.PIPE).communicate()[0]
+
     # ./ilastik-1.1.7-Linux/bin/python train_headless.py MyNewProject.ilp /tmp/cell-slide.png
 
     # for each image retrain model
