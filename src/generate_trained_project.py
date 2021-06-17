f5b4bb3b503977c3aed0a1c47d6b812182bb40b9
DVMix <dvmixalkin@mail.ru>
2021-06-17 09:32:02 +0300

    fix args parcer list

--- src/generate_trained_project.py
+++ src/generate_trained_project.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import os
 import numpy
 import argparse
+import json
 from ilastik import app
 from lazyflow.graph import Graph
 from lazyflow.roi import roiToSlice, roiFromShape
@@ -81,6 +82,7 @@ def generate_trained_project_file(project_output_path, raw_data_dir, label_data_
         opPixelClassification.LabelInputs[lane][entire_volume_slicing] = label_volume
 
     assert max_label_class > 1, "Not enough label classes were found in your label data."
+
     opPixelClassification.LabelNames.setValue(label_names)
     opPixelClassification.LabelColors.setValue(label_colors)
     opPixelClassification.FreezePredictions.setValue(False)
@@ -92,8 +94,8 @@ parser = argparse.ArgumentParser(description="ilastik trained project file")
 parser.add_argument("--save_ilp_to",       default="",  type=str,  help="output path to ilastik project")
 parser.add_argument("--train_images_dir",  default="",  type=str,  help="path to train images directory")
 parser.add_argument("--machine_masks_dir", default="",  type=str,  help="path to machine masks directory")
-parser.add_argument("--label_names",       default=[],  type=list, help="class names in Supervisely project")
-parser.add_argument("--label_colors",      default=[],  type=list, help="class colors in Supervisely project")
+parser.add_argument("--label_names",       nargs="+",   default=[], help="class names in Supervisely project")
+parser.add_argument("--label_colors",      nargs="+",   default=[], help="class colors in Supervisely project")
 parser.add_argument("--num_tress_total",   default=100, type=int,  help="total number of trees")
 
 args = parser.parse_args()
@@ -103,3 +105,5 @@ generate_trained_project_file(args.save_ilp_to,
                               args.label_names,
                               args.label_colors,
                               args.num_tress_total)
+
+# usage example: python generate_trained_project.py --save_ilp_to /path/to/lip_folder --label_names a b c
