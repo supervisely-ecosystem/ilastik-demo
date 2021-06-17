7dac0240c640e4d3659151d65abe0b526afd80bf
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2021-06-17 11:23:44 +0300

    remote dev - wip

--- docker/start.sh
+++ docker/start.sh
@@ -4,6 +4,7 @@
 #docker run --rm -it -p 7777:22 --shm-size='1G' -e PYTHONUNBUFFERED='1' $IMAGE"-debug"
 # -v ~/max:/workdir
 
+docker build -t supervisely/ilastik:1.0.0 . && \
 cp /root/.ssh/authorized_keys . && \
 docker-compose up -d && \
 docker-compose ps
