5ca31184fed7d5705b2c1ccb58b8da06637560b7
cxnt <pavelbartsits@gmail.com>
2021-07-06 18:15:51 +0300

    correct target classes for existing project fix

--- src/ui/target_classes.py
+++ src/ui/target_classes.py
@@ -4,6 +4,11 @@ import mode_selector as ms
 
 def init(data, state):
     state["classesInfo"] = []
-    for obj_class in g.project_meta.obj_classes:
-        if obj_class.name in ms.selected_classes:
-            state["classesInfo"].append(obj_class.to_json())
+    if g.mode == "Create new Project":
+        for obj_class in g.project_meta.obj_classes:
+            if obj_class.name in ms.selected_classes:
+                state["classesInfo"].append(obj_class.to_json())
+    else:
+        for obj_class in ms.project_meta:
+            if obj_class.name in ms.selected_classes:
+                state["classesInfo"].append(obj_class.to_json())
