67392d9bac3f7baeb0188dd3648f88b1ba04270b
cxnt <pavelbartsits@gmail.com>
2021-07-07 14:25:16 +0300

    launch app from instance

--- src/main.py
+++ src/main.py
@@ -59,6 +59,8 @@ def main():
     init_ui.init(data, state)
 
     g.my_app.compile_template(root_source_dir)
+    sly.logger.debug(f"DATA: {data}")
+    sly.logger.debug(f"STATE: {state}")
     g.my_app.run(data=data, state=state)
 
 
