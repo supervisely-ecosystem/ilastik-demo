03ab9d9a057953e249c38da012acc6f4ee04331e
cxnt <pavelbartsits@gmail.com>
2021-07-07 16:37:09 +0300

    Existing project mode fix

--- src/mode_selector.py
+++ src/mode_selector.py
@@ -48,6 +48,5 @@ else:
         raise Exception("At least 2 classes must be selected")
     g.project_meta = g.project_meta.merge(ex_meta)
     g.api.project.update_meta(g.project_id, g.project_meta.to_json())
-    g.api.task.set_field(g.task_id, "state.loading", False)
 
 machine_map = {obj_class: [idx, idx, idx] for idx, obj_class in enumerate(selected_classes, start=1)}
