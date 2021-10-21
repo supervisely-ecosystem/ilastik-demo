caf6142e97a8888dc7615364384dafbb2e378b68
cxnt <pavelbartsits@gmail.com>
2021-10-21 13:55:44 +0300

    correct sly version in docker

--- docker/start.sh
+++ docker/start.sh
@@ -4,7 +4,7 @@
 #docker run --rm -it -p 7777:22 --shm-size='1G' -e PYTHONUNBUFFERED='1' $IMAGE"-debug"
 # -v ~/max:/workdir
 
-docker build -t supervisely/ilastik:1.0.0 . && \
+docker build -t supervisely/ilastik:6.1.88 . && \
 cp /root/.ssh/authorized_keys . && \
 docker-compose build && \
 docker-compose up -d && \
